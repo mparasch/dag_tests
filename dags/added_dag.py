49035bc4deb85315ae5678d317319a917ccb6c1c
mparasch <76914236+mparasch@users.noreply.github.com>
2024-09-22 15:30:26 -0500

    Create added_dag.py

---
+++ dags/added_dag.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+# Making Chagnes for git-sync
+
+def helloWorld():
+    print('Hello World')
+
+with DAG(dag_id="test_add",
+         start_date=datetime(2021,1,1),
+         schedule_interval="@hourly",
+         catchup=False) as dag:
+    
+    task1 = PythonOperator(
+            task_id="test_add",
+            python_callable=helloWorld)
+
+task1
